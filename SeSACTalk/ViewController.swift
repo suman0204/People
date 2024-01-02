3af9135a8c853ae6d4c1b522cc684bad5a3fcddf
sumanhong <suman0204@naver.com>
2024-01-02 14:29:54 +0900

    Initial Commit

---
+++ SeSACTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACTalk
+//
+//  Created by 홍수만 on 2024/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
